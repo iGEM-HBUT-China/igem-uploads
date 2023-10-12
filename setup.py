04133731857a5aea006edd0223830f74f9916cce
lty2002 <liangtianyi2002@foxmail.com>
2023-10-12 15:36:12 +0800

    v1.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name="igem-uploads",
-    version="1.1.4",
+    version="1.2.0",
     author="iGEM-HBUT-China",
     author_email="liangtianyi2002@outlook.com",
     description="Helps iGEMers upload their files to the iGEM server.",
